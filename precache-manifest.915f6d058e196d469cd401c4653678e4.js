2d31303d55f104b74e03520c0b9b1c30b9454e88
Alyona Malinina <alenam2803@mail.ru>
2020-03-24 21:01:24 +0300

    Updates

---
+++ precache-manifest.915f6d058e196d469cd401c4653678e4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8640dea712048a94eee09e44258464f3",
+    "url": "/social-application/index.html"
+  },
+  {
+    "revision": "30cfb22825c2ee84ddfe",
+    "url": "/social-application/static/css/4.af6fe4c1.chunk.css"
+  },
+  {
+    "revision": "2b1f0cb61f50acccff19",
+    "url": "/social-application/static/css/5.3028d20a.chunk.css"
+  },
+  {
+    "revision": "89567383e4efe938efe0",
+    "url": "/social-application/static/css/6.ab20ba97.chunk.css"
+  },
+  {
+    "revision": "2a0e75267b5cb8244059",
+    "url": "/social-application/static/css/7.71d3bd8b.chunk.css"
+  },
+  {
+    "revision": "fc2d60525f83280f2016",
+    "url": "/social-application/static/css/main.57007253.chunk.css"
+  },
+  {
+    "revision": "d140fea29c87a7e665d7",
+    "url": "/social-application/static/js/0.24574374.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/social-application/static/js/0.24574374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cecae5f0ba99488f2cc",
+    "url": "/social-application/static/js/3.aaba6940.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-application/static/js/3.aaba6940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30cfb22825c2ee84ddfe",
+    "url": "/social-application/static/js/4.9ac7157d.chunk.js"
+  },
+  {
+    "revision": "2b1f0cb61f50acccff19",
+    "url": "/social-application/static/js/5.3e9b1e0f.chunk.js"
+  },
+  {
+    "revision": "89567383e4efe938efe0",
+    "url": "/social-application/static/js/6.f2a82e72.chunk.js"
+  },
+  {
+    "revision": "2a0e75267b5cb8244059",
+    "url": "/social-application/static/js/7.71ea0ec4.chunk.js"
+  },
+  {
+    "revision": "fc2d60525f83280f2016",
+    "url": "/social-application/static/js/main.7cacbbf5.chunk.js"
+  },
+  {
+    "revision": "3d9708f771a1be925d6b",
+    "url": "/social-application/static/js/runtime-main.50510614.js"
+  }
+]);
